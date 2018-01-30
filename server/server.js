38c516674d23373ad79470f2d62e4307138bf584
mikeBridge1993 <miguelponte93@hotmail.com>
2018-01-30 13:25:38 -0100

    Add /todos/:id

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const {mongoose} = require("./db/mongoose");
+const {ObjectID} = require('mongodb');
 const {todo} = require("./models/todo");
 const {user} = require("./models/user");
 
@@ -26,6 +27,24 @@ app.get('/todos', (req, res) => {
     
     todo.find().then((todos) => {
         res.send({todos});  
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+
+app.get('/todos/:id', (req, res) => {
+    
+    if(!ObjectID.isValid(req.params.id)){ 
+        res.status(404).send("ID not valid");
+    }
+    
+    todo.findById(req.params.id).then((todos) => {
+        if(todos){
+            res.send({todos});     
+        }else{
+            res.status(404).send("ID not found in database");
+        }   
     }, (e) => {
         res.status(400).send(e);
     });
