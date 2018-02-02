b7b1ddfdb2ae72fa574916e8910d8d564f370091
mikeBridge1993 <miguelponte93@hotmail.com>
2018-02-02 18:33:35 -0100

    Add logout or DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -72,6 +72,19 @@ UserSchema.statics.findByToken = function (token) {
     });
 };
 
+
+UserSchema.methods.removeToken = function (token) {
+    var user = this;
+    
+    return user.update({
+        $pull: {
+            tokens: {
+                token: token
+            }
+        }  
+    });
+};
+
 UserSchema.statics.findByCredentials = function (email, password) {
     var User = this;
     
